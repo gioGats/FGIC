6aa66f9e44dbfabe04946d4a713fd5dea25bc19b
gioGats <ryangiarusso@gmail.com>
2016-11-16 18:46:51 -0600

    Finish midcar_dict

--- make_midcars_dict.py
+++ make_midcars_dict.py
@@ -1,15 +1,26 @@
 import os
 import numpy as np
+import pickle
+from random import gauss
 
 
 def make_midcars_dict():
     dog_values = []
     for f in os.listdir('/home/rgio/FGIC/dogs/dog_photos'):
         dog_values.append(len(os.listdir('/home/rgio/FGIC/dogs/dog_photos/%s' % f)))
-    print(dog_values)
 
-    print(np.mean(dog_values))
-    print(np.std(dog_values))
+    mu = np.mean(dog_values)
+    sigma = np.std(dog_values)
+
+    midcars_dict = {}
+    for f in os.listdir('/home/rgio/FGIC/midcars/car_photos'):
+        midcars_dict[f] = gauss(mu, sigma)
+
+    with open('midcars_dict.pkl', 'wb') as f:
+        pickle.dump(midcars_dict, f)
+        f.close()
+
+    print(midcars_dict)
 
 if __name__ == '__main__':
     make_midcars_dict()
