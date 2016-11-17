e5c0296bff37349f74eaaae0d623e453995c6485
Ryan Giarusso <gioGats@users.noreply.github.com>
2016-11-16 18:24:09 -0600

    Update and rename cars_100_run.sh to midcars_run.sh

---
+++ midcars_run.sh
@@ -0,0 +1,10 @@
+cd ~/tensorflow
+
+python3 tensorflow/examples/image_retraining/retrain.py \
+--bottleneck_dir=/home/rgio/FGIC/midcars/bottlenecks \
+--how_many_training_steps 10000 \
+--model_dir=/home/rgio/FGIC/midcars/inception \
+--output_graph=/home/rgio/FGIC/midcars/retrained_graph.pb \
+--output_labels=/home/rgio/FGIC/midcars/retrained_labels.txt \
+--learning_rate=0.003 \
+--image_dir /home/rgio/FGIC/midcars/car_photos
